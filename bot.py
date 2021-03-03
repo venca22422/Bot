3bd90f5b00add28d02c17845b7f8a58f6be1952a
venca22422 <79199501+venca22422@users.noreply.github.com>
2021-03-03 12:50:07 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,68 +2,103 @@
 import discord
 from discord.ext import commands, tasks
 from discord.utils import get
+from discord.voice_client import VoiceClient
 import random
+import asyncio
+from time import sleep
 
+#Ověrění
+def is_it_me(ctx):
+    return ctx.author.id == 710059910783697026
 
 #Spuštění bota:
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
 client = commands.Bot(command_prefix = '.', intents = intents)
 client.remove_command("help")
+token = ("ODExNjE2MDc4MTY5MjQzNjQ5.YC0yYA.Q-QRXESdu3MNpYKra89HjGSRYV0")
 
 @client.event
 async def on_ready():
-    print("Bot online!")
+    print("Jsem online!!!")
     await client.change_presence(status=discord.Status.idle, activity=discord.Game("si na serveru"))
-
-
+   
 #help
 @client.command(pass_context=True)
 async def help(ctx):
     embed = discord.Embed(
-        colour = discord.Colour.orange()
+        colour = discord.Colour.blue()
     )
-    embed.set_author(name="Help")
-    embed.add_field(name=".otázka", value="Odpoví na položenou otázku", inline=False)
+    embed.set_author(name="   ---------------  Help  ---------------  ")
+    embed.add_field(name=".rozhovor", value="Odpoví na položenou otázku", inline=False)
+    embed.add_field(name=".mampravdu", value="Odpoví Ano/Ne", inline=False)
+    embed.add_field(name=".cislo", value="Vygeneruje random číslo 0-9", inline=False)
     embed.add_field(name=".úklid (počet)", value="vymaže zprávy", inline=False)
 
     await ctx.send(embed=embed)
 
 #Připojení/Odpojení člověka:
 @client.event
-async def on_member_join(ctx, member):
-    await ctx.send("Ahoj {member}, vítáme tě u nás na serveru! :grin:")
+async def on_member_join(member):
+    channel = discord.utils.get(member.guild.channels, name='hlavní-chat')
+    await channel.send(f'Vítej {member.mention}! Kdyby jsi potřeboval pomoci můžeš dát ".help"!, nebo napsat do Supportu rádi ti pomůžeme...')
+##    user = member
+##    role = discord.utils.get(guild.roles, name="Neověřeno!")
+##    await user.add_roles(role)
 
 @client.event
-async def on_member_remove(ctx, member):
-    await ctx.send("Člen {member} nás opustil. :sob:")
+async def on_member_remove(member):
+    channel = discord.utils.get(member.guild.channels, name='hlavní-chat')
+    await channel.send(f'Člen {member.mention} nás opustil. :sob:')
 
-#Ověrění
-def is_it_me(ctx):
-    return ctx.author.id == 710059910783697026
+##################################   příkazy pro členy   ######################################                   
 
-#Kommandy:
-#.otázka
+#.rozhovor
 @client.command()
-async def otázka(ctx, *, question):
-    responses = ["Super",
+async def rozhovor(ctx, *, question):
+    seznam1 = ["Dobrý",
+                 "Dobře ty",
+                 "Mám se dobře",
+                 "Jsi borec",
+                 "Nevim",
                  "Super",
-                 "A dál?",
-                 "Hustý",
-                 "Hustý",
-                 "Mám se dobře",
-                 "Mám se dobře",
+                 ":joy:",
+                 "Juj"]
+    await ctx.send(f"Otázka: {question}\nOdpověď: {random.choice(seznam1)}")
+
+
+#.mampravdu
+@client.command()
+async def mampravdu(ctx, *, question):
+    seznam1 = ["Možná",
                  "Ano",
                  "Ano",
                  "Ano",
                  "Ne",
                  "Ne",
                  "Ne"]
-    await ctx.send(f"Otázka: {question}\nOdpověď: {random.choice(responses)}")
+    await ctx.send(f"Otázka: {question}\nOdpověď: {random.choice(seznam1)}")
+
+#.cislo
+@client.command()
+async def cislo(ctx):
+    cisla = [    "1",
+                 "2",
+                 "3",
+                 "4",
+                 "5",
+                 "6",
+                 "7",
+                 "8",
+                 "9",
+                 "0"]
+    await ctx.send(f"{random.choice(cisla)}")
 
 #.úklid
 @client.command()
-async def úklid(ctx, amount=5):
+async def úklid(ctx, amount=4):
     await ctx.channel.purge(limit=amount)
+
+###############################   správa serveru   ###############################################
 
 #.kick
 @client.command()
@@ -93,68 +128,34 @@ async def unban(ctx, *, member):
             return
 
 
+#Vytvoř/smaž textový kanál
 @client.command()
-async def vytvořkanál(ctx, channelName):
+async def ahoj(ctx):
     guild = ctx.guild
-    await guild.create_text_channel(name="{}".format(channelName))
+    username = ctx.message.author.name
+    his_name = get(guild.members, name=username)
+    name = 'Příkazy pro boty'
+    category = discord.utils.get(ctx.guild.categories, name=name)
+    channel = await ctx.guild.create_text_channel(username, category=category)
+    await channel.set_permissions(guild.default_role, view_channel=False, send_messages=False)
+    await channel.set_permissions(guild.me, view_channel=True, send_messages=True)
+    await channel.set_permissions(his_name, view_channel=True, send_messages=True)
+    user = ctx.message.author
+    role = discord.utils.get(guild.roles, name="Dřevo")
+    await user.add_roles(role)
+    user = ctx.message.author
+    role = discord.utils.get(guild.roles, name="Neověřeno!")
+    await user.remove_roles(role)
+    await ctx.channel.purge(limit=1)
 
 @client.command()
-async def smažkanál(ctx, channel: discord.TextChannel):
+@commands.check(is_it_me)
+async def delete(ctx, channel: discord.TextChannel):
     await channel.delete()
 
-###.připoj
-##@client.command(pass_content=True)
-##async def připoj (ctx):
-##    channel = ctx.message.author.voice.channel
-##    if not channel:
-##        await ctx.send("You are not connected to a voice channel")
-##        return
-##    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-##    if voice and voice.is_connected():
-##        await voice.move_to(channel)
-##    else:
-##        voice = await channel.connect()
-##
-###.odpoj
-##@client.command(pass_context = True)
-##async def odpoj(ctx):
-##    channel = ctx.message.author.voice.channel
-##    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-##    if voice and voice.is_connected():
-##        await voice.disconnect()
-##    else:
-##        await voice.disconnect()
-##        await ctx.send("Nejsem v hlasovém kanálu!")
-##
-##
-###.pause
-##@client.command()
-##async def pause(ctx):
-##    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-##    voice.pause()
-##
-###.pokračuj
-##@client.command()
-##async def pokračuj(ctx):
-##    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-##    if voice.is_paused():
-##        voice.resume()
-##    else:
-##        await ctx.send("Nic není pauznuté!")
-##
-###.stop
-##@client.command()
-##async def stop(ctx):
-##    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-##    if voice.is_connected():
-##        voice.stop
-##    else:
-##        await ctx.send("Nepřehrávám audio!")
 
-       
 #Klient:
-client.run("ODExMjQzMjM3MTkyMzY4MjE5.YCvXIw.YcBO0s58tlbq4irKDzG32IwEwY8")
-
+client.run(token)
 
 
 
